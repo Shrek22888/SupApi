aee3b088745162148ae673cd1166699cb8ab7d26
Shrek2888 <nikita.lyubimow2014@yandex.ru>
2020-06-22 11:52:47 +0300

    init

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SupApi.ViewModels;
+
+namespace SupApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        public async Task<ActionResult> Get()
+        {
+            var user = new UserVm("dsad", "dasdsa", "ddsad", "22222");
+            await Task.Delay(1000);
+
+            ContentResult result = new ContentResult();
+            result.ContentType = "application/json";
+
+            result.Content = JsonConvert.SerializeObject(user);
+            return result;
+        }
+    }
+}
---
+++ ViewModels/UserVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupApi.ViewModels
+{
+    public class UserVm
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Number { get; set; }
+
+        public UserVm(string userName, string firstName, string secondName, string number)
+        {
+            UserName = userName;
+            FirstName = firstName;
+            SecondName = secondName;
+            Number = number;
+        }
+    }
+}
